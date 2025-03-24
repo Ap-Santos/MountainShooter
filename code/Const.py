0402953219f1ef0d40349acd24b3b07bd6bbba83
Ariel Santos <arielpereirasantos104@gmail.com>
2025-03-24 02:06:06 -0300

    Creating menu interaction Creating ABC

--- code/Const.py
+++ code/Const.py
@@ -1,7 +1,7 @@
 #C
 C_ORANGE = (255, 128, 0)
 C_WHITE =(255,255,255)
-
+C_YELLOW = (238,173,45)
 
 #M
 MENU_OPTION = ('NEW GAME 1P',
@@ -9,6 +9,19 @@ MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COMPETITIVE',
                'SCORE',
                'EXIT')
+
+#E
+ENTITY_SPEED ={
+    'Level1Bg0': 0,
+    'Level1Bg1': 1,
+    'Level1Bg2': 2,
+    'Level1Bg3': 3,
+    'Level1Bg4': 4,
+    'Level1Bg5': 5,
+    'Level1Bg6': 6
+}
+
+
 
 
 #W
--- code/Game.py
+++ code/Game.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 import pygame
 
-from code.Const import WIN_WIDTH, WIN_HEIGHT
+from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
 from code.Menu import Menu
+from code.Level import Level
 
 
 class Game:
@@ -12,8 +13,21 @@ class Game:
         self.screen = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
-
         while True:
             menu = Menu(self.screen)
-            menu.run()
-            pass
+            menu_return = menu.run()
+
+            if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
+                level = Level(self.screen, 'Level1', menu_return)
+                level_return = level.run()
+            elif menu_return == MENU_OPTION[4]:
+                pygame.quit()  # close Window
+                quit()  # end pygame
+            else:
+                pass
+
+
+
+
+
+
